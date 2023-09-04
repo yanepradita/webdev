1637dab02233c4928011d9b03b3f7b11c16a443a
yanepradita <yanepradita821@gmail.com>
2023-09-04 17:54:51 +0700

    update tugas 2 mission 2

--- mission2/tugas_2/Tugas2.js
+++ mission2/tugas_2/Tugas2.js
@@ -22,9 +22,9 @@ const char = document.getElementById("kucing");
         }
 
         const ifHitBola = setInterval(function(){
-            const charTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"))
+            const kucingTop = parseInt(window.getComputedStyle(char).getPropertyValue("top"))
             const bolaLeft = parseInt(window.getComputedStyle(bola).getPropertyValue("left"))
-        if(bolaLeft < 90 && bolaLeft > 0 && charTop >= 90){
+        if(bolaLeft < 90 && bolaLeft > 0 && kucingTop >= 75){
             bola.style.animation = "none"
             bola.style.display = "none"
             if(confirm ("Meow mengenai bola, main lagi?")){
